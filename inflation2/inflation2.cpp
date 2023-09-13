13d23c121e511bc0df183e05b314e6479e3391d5
Hjalte Dalland <hjaltesorgenfrei@gmail.com>
2023-09-13 22:37:16 +0200

    0.14

---
+++ inflation2/inflation2.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <unordered_map>
+#include <string>
+
+typedef long long LL;
+
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL); 
+    int n, q;
+    cin >> n;
+    LL sum = 0;
+    unordered_map<LL, int> m;
+    for (int i = 0; i < n; i++) {
+        int a;
+        cin >> a;
+        m[a] = m[a] + 1;
+        sum += a;
+    }
+    LL inflation = 0;
+    cin >> q;
+    int a, b;
+    for (int i = 0; i < q; i++) {
+        cin.get();
+        if(cin.get() == 'S') {
+            cin.ignore(3);
+            cin >> a >> b;
+            int count = m[a - inflation];
+            m[a - inflation] = 0;
+            m[b - inflation] += count;
+            sum += (b - a) * count;
+        }
+        else {
+            cin.ignore(9);
+            cin >> a;
+            inflation += a;
+            sum += a * n;
+        }
+        cout << sum << "\n";
+    }
+}
